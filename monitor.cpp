4fe40b7c8418cd5f404389a75e9befc796688bfe
pavanshenoy89 <71321360+pavanshenoy89@users.noreply.github.com>
2024-09-22 20:21:09 +0530

    Add files via upload

--- monitor.cpp
+++ monitor.cpp
@@ -4,34 +4,32 @@
 #include <chrono>
 #include <iostream>
 #include <vector>
-using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds ;
-using std::string, std::vector ,std::pair;
+using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+using std::string, std::vector, std::pair;
 
 vector<string> vitalType = { "Temperature", "Pulse Rate", "SPO2" };
 
 vector<pair<float, float>> vitalLimits = {
-    {95, 102}, // Temperature
-    {60, 100}, // Pulse Rate
-    {90, 100}  // SPO2
+    {95, 102},  // Temperature
+    {60, 100},  // Pulse Rate
+    {90, 100}   // SPO2
 };
 
-bool isVitalsOutOfRange(float vitalValue, pair<float, float> vitalLimits)
-{
-    if (vitalValue < vitalLimits.first || vitalValue > vitalLimits.second)
-    {
+bool isVitalsOutOfRange(float vitalValue, pair<float, float> vitalLimits){
+    if (vitalValue < vitalLimits.first || vitalValue > vitalLimits.second){
         return true;
     }
     return false;
 }
 
-void displayWarningMessage(string vitalType) {
-  cout << vitalType << " is out of range!\n";
-  for (int i = 0; i < 6; i++) {
-    cout << "\r* " << flush;
-    sleep_for(seconds(1));
-    cout << "\r *" << flush;
-    sleep_for(seconds(1));
-  }
+void displayWarningMessage(const string& vitalType) {
+    cout << vitalType << " is out of range!\n";
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
